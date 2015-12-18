89a51029e783b6bb721b3f1e6d0c8909d8900833
Robat Das <orvi2014@users.noreply.github.com>
2015-12-19 03:34:39 +0600

    Update image.py

--- image.py
+++ image.py
@@ -6,4 +6,7 @@ image_file = Image.open("image.jpg")
 # convert image to black and white
 image_file = image_file.convert('1') 
 
+#another way you can rewrite this line
+#image_file = Image.open("image.jpg").convert("L")
+
 image_file.save('image.jpg')
